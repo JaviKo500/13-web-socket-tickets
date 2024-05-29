93394726deaa7546aa5fe1bee92025f7b03f2f99
JaViKo500 <javikogutierrez64@gmail.com>
2024-05-28 22:43:58 -0500

    init ws

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import { createServer } from 'http';
 import { envs } from './config/envs';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
+import { WssServices } from './presentation/services/wss.services';
 
 
 (async()=> {
@@ -15,5 +17,7 @@ function main() {
     routes: AppRoutes.routes,
   });
 
+  const httpServer = createServer( server.app );
+  WssServices.initWss( { server: httpServer } );
   server.start();
 }
