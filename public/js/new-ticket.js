eed5cdd8f62ed02f685ba5aed18fbeb016aec0fc
JaViKo500 <javikogutierrez64@gmail.com>
2024-06-01 08:38:30 -0500

    refactor code

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -5,7 +5,7 @@ const btnAddTicket = document.getElementById('btn-add-ticket');
 
 getLastNumber();
 async function getLastNumber() {
-   const resp = await fetch('http://localhost:3000/api/ticket/last', {
+   const resp = await fetch('/api/ticket/last', {
       method: 'GET',
    });
    const { lastTicketNumber = 0 } = await resp.json();
@@ -13,11 +13,13 @@ async function getLastNumber() {
    lbNewTicket.innerHTML = lastTicketNumber;
 }
 
-
-btnAddTicket.addEventListener('click', async ( event ) => {
-   const resp = await fetch('http://localhost:3000/api/ticket/', {
+async function createTicket() {
+   const resp = await fetch('/api/ticket', {
       method: 'POST',
    });
    const { ticket = {} } = await resp.json();
    lbNewTicket.innerHTML = ticket?.number ?? 0;
-});
+}
+
+
+btnAddTicket.addEventListener('click', async ( event ) => await createTicket());
